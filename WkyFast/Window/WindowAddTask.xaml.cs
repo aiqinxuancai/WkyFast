9ea6cafceafe4185814a8a6da9c620c9c8687ae4
aiqinxuancai <aiqinxuancai@163.com>
2021-11-14 14:31:51 +0800

    支持添加bt文件

--- WkyFast/Window/WindowAddTask.xaml.cs
+++ WkyFast/Window/WindowAddTask.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,27 +38,98 @@ namespace WkyFast.Window
             dialog.ShowDialog();
         }
 
-        private async void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        private async Task<bool> RunUrlDownload(string url)
         {
-            ConfirmButton.IsEnabled = false;
-            //TODO 支持选择设备和磁盘？？
-            //WkyAccountManager.WkyApi.CreateTaskWithUrlResolve();
-            var urlResoleResult = await WkyApiManager.WkyApi.UrlResolve(WkyApiManager.NowDevice.Peerid, UrlTextBox.Text);
+            var urlResoleResult = await WkyApiManager.WkyApi.UrlResolve(WkyApiManager.NowDevice.Peerid, url);
             if (urlResoleResult.Rtn == 0)
             {
                 var createResult = await WkyApiManager.WkyApi.CreateTaskWithUrlResolve(WkyApiManager.NowDevice.Peerid, WkyApiManager.GetUsbInfoDefPath(), urlResoleResult);
                 if (createResult.Rtn == 0)
                 {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private async Task<bool> RunBtFileDownload(string filePath)
+        {
+            var btResoleResult = await WkyApiManager.WkyApi.BtCheck(WkyApiManager.NowDevice.Peerid, filePath);
+            if (btResoleResult.Rtn == 0)
+            {
+                var createResult = await WkyApiManager.WkyApi.CreateTaskWithBtCheck(WkyApiManager.NowDevice.Peerid, WkyApiManager.GetUsbInfoDefPath(), btResoleResult);
+                if (createResult.Rtn == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private async void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConfirmButton.IsEnabled = false;
+            //TODO 支持选择设备和磁盘？？
+            //WkyAccountManager.WkyApi.CreateTaskWithUrlResolve();
+
+            try
+            {
+                var result = await RunUrlDownload(UrlTextBox.Text);
+                if (result)
+                {
                     this.Close();
                 }
             }
-
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("添加异常，请重试", ex.ToString());
+            }
             ConfirmButton.IsEnabled = true;
         }
+
+
+
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private async void UrlTextBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                var file = files[0];
+                //HandleFile(file);
+
+                //判断是不是BT
+                if (!file.EndsWith(".torrent"))
+                {
+                    return;
+                }
+
+                try
+                {
+                    var result = await RunBtFileDownload(file);
+                }
+                catch (Exception ex)
+                {
+                    await this.ShowMessageAsync("添加异常，请重试", ex.ToString());
+                }
+                
+            }
+            else if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                //粘贴上去？
+            }
+        }
+
+        private void UrlTextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
