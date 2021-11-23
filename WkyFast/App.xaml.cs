f9a3ab4afb7ec088ee99c2202b1bd629efbdb2f5
aiqinxuancai <aiqinxuancai@163.com>
2021-11-23 13:12:49 +0800

    update

--- WkyFast/App.xaml.cs
+++ WkyFast/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +15,18 @@ namespace WkyFast
     /// </summary>
     public partial class App : Application
     {
+        App()
+        {
+            //try
+            //{
+            //    JObject root = new JObject();
+            //    string aaa = root["aaa"].ToString();
+
+            //} 
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //}
+        }
     }
 }
