9d94982bab333640ec6e21d0a6ce3c06dd4acfdc
aiqinxuancai <aiqinxuancai@163.com>
2022-11-07 11:06:17 +0800

    增加全局seh

--- WkyFast/App.xaml.cs
+++ WkyFast/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using WkyFast.Service;
 
 namespace WkyFast
 {
@@ -18,6 +19,8 @@ namespace WkyFast
     {
         static App()
         {
+
+
             TextOptions.TextFormattingModeProperty.OverrideMetadata(typeof(Window),
                 new FrameworkPropertyMetadata(TextFormattingMode.Display, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
         }
@@ -28,6 +31,28 @@ namespace WkyFast
                 WkyFast.MainWindow.Instance.Close();
             }
             App.Current.Shutdown();
+        }
+
+
+        App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            EasyLogManager.Logger.Error(ex);
+            MessageBox.Show(ex?.Message + Environment.NewLine + ex?.InnerException?.ToString(), "Error#1", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            EasyLogManager.Logger.Error(e?.ToString());
+            MessageBox.Show(e?.Exception?.Message + Environment.NewLine + e?.Exception?.InnerException?.ToString(), "Error#2", MessageBoxButton.OK, MessageBoxImage.Information);
+            e.Handled = true;
         }
     }
 }
